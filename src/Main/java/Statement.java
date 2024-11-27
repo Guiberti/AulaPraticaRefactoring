7ca0521d207712b66a6a6425b53ced366e88c0d5
Guilherme Dudu Berti <guiberti1227@gmail.com>
2024-11-26 23:29:55 -0300

    Commit 17

--- src/Main/java/Statement.java
+++ src/Main/java/Statement.java
@@ -1,4 +1,22 @@
 package Main.java;
 
-public class Statement {
+import java.util.Enumeration;
+
+public abstract class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerString(aCustomer);
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += rentalString(each);
+        }
+
+        result += footerString(aCustomer);
+        return result;
+    }
+
+    abstract String headerString(Customer aCustomer);
+    abstract String rentalString(Rental aRental);
+    abstract String footerString(Customer aCustomer);
 }
